482265c99debe79f41973d93593ee683455a4177
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-23 21:47:23 +0530

    Uploaded

--- src/Productpage/Productpg.js
+++ src/Productpage/Productpg.js
@@ -5,12 +5,13 @@ import Rightpanel from "./Rightpanel";
 const Productpg = () => 
 {
   return (
-       <div className = "main_productpg"     style =  {{display:'flex'}}>
-           <div className = "left_product"    style =  {{width:'30%',backgroundColor:'lightgreen'}}>
+       <div className = "main_productpg"        style =  {{display:'flex'}}>
+             <div className = "left_product"    style =  {{width:'30%',backgroundColor:'lightgreen'}}>
                 <Leftpanel />
-            </div> 
+             </div> 
             <div className = "right_product"  style =  {{width:'70%',backgroundColor:'lightblue'}}>
-               <div className="main_rightpanel"  style = {{display:'flex',flexDirection:'row',flexWrap:'wrap'}}>
+               <div className = "main_rightpanel"  style = {{display:'flex',flexDirection:'row',
+               flexWrap:'wrap'}}>
                   <Rightpanel />
                   <Rightpanel />
                   <Rightpanel />
