9811e8d1eecd12b81ab1f39be0833142e273fb6c
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-23 16:44:03 +0530

    Uploaded

---
+++ src/Components/Advertisement.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Advertisement = () => 
+{
+  return (
+    <>
+         <div className = "main_advertisement">
+               <div className="add_header"></div>
+               <div className="add_body"></div>
+               <div className="add_footer"></div>
+        </div>
+    </>
+  )
+};
+
+export default Advertisement;
