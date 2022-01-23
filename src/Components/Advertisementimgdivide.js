0f4b53da178641327d445ab85ac0a80635e2f46a
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-23 19:29:16 +0530

    Uploaded

--- src/Components/Advertisementimgdivide.js
+++ src/Components/Advertisementimgdivide.js
@@ -8,9 +8,9 @@ const Advertisementimgdivide = () =>
                      <span style = {{fontSize:'22px', fontWeight:'bolder',color:'black'}}>   Upgrade your home | Amazon Brands & more   </span>  
                 </div>
                 <div className="add_body" style = {{flexWrap:'wrap',display:'flex'}}>
-                        <img src = "/images/smartled1.jpg" style = {{width:'45%' , margin:'2%' }}  />
+                        <img src = "/images/smartled1.jpg"  style = {{width:'45%' , margin:'2%' }}  />
                          <img src = "/images/smartled2.jpg" style = {{width:'45%', margin:'2%'}}/>
-                         <img src = "/images/smartled3.jpg"style = {{width:'45%' , margin:'2%'}}/>
+                         <img src = "/images/smartled3.jpg" style = {{width:'45%' , margin:'2%'}}/>
                          <img src = "/images/smartled4.jpg" style = {{width:'45%' , margin:'2%'}}/>
                 </div>
                 <div className = "add_footer">
