fbbd265c77decdc46955517b72738736666ff8cb
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-24 06:53:50 +0530

    uploaded

--- src/Productpage/Productlist.js
+++ src/Productpage/Productlist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rightpanel from './Rightpanel';
 
 const Productlist = (props) =>
  {
--- src/Productpage/Rightpanel.js
+++ src/Productpage/Rightpanel.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React , { useState , useEffect }from 'react';
 import Productlist from  "./Productlist";
 const Rightpanel = () => 
 {
-  return (
-      <>
-        <Productlist  rating = "11111111" price = "9999999" 
-                  name = "iphone 13"     img  = "/images/Img1.webp"/>
-        <Productlist   rating = "22222222" price = "8888888" 
-                  name = "iphone 12"    img  = "/images/Img7.webp" />
-        <Productlist  rating = "33333333" price = "7777777"
-                   name = "iphone 11"   img  = "/images/Img3.webp"  />
-        <Productlist  rating = "44444444" price = "6666666"
-                   name = "iphone 10"   img  = "/images/Img4.webp" />
+  const[listofproduct,setlistofproduct] = useState([]);
 
-      </>
+   useEffect(() =>
+   {
+        let list = 
+        [
+          { rating : "11111111" ,   price : "9999999" ,    name : "iphone 13"  },
+          { rating : "2222222"  ,   price : "88888" ,      name : "iphone 12"  },
+          { rating : "3333333"  ,   price  : "777777" ,    name : "iphone 11"  },
+          { rating : "44444444" ,   price : "6666666666" , name : "iphone 10"  }
+        ]
+          //  setlistofproduct(list);
+          // console.log(listofproduct);
+   });
+
+  return (
+          <div className='inside'>
+        {
+            listofproduct.map((item) =>
+              {
+               <Productlist rating = {item.rating} price = {item.price} name = {item.name} />
+              })
+        }
+         </div>
   )
 };
 
