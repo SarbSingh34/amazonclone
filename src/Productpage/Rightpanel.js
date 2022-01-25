62f400b4154e4e3491b46d90e9e05e7d4999d8b7
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-25 16:34:19 +0530

    upp

--- src/Productpage/Rightpanel.js
+++ src/Productpage/Rightpanel.js
@@ -10,12 +10,12 @@ const Rightpanel = () =>
    {
            let list = 
         [
-          {   id : 1111111   ,  rating : "8888888"  ,   price : "9999999" , name : "iphone 13" , image : "/images/iPhone 13.jpg" },
-          {   id : 22222222  ,  rating : "2222222"  ,   price : "8888888" , name : "iphone 12" , image : "/images/iPhone  11.jpg" },
-          {   id : 33333333  ,  rating : "3333333"  ,   price : "7777767" , name : "iphone 11" , image : "/images/iPhone 12.jpg" },
-          {   id : 4444444   ,  rating : "444444"  ,   price : "6666666" , name : "iphone 10" , image : "/images/iPhone 10.png" },
-          {   id : 5555555  ,  rating : "5555555"  ,   price : "5555555" , name :  "iphone 8"  , image : "/images/iPhone 8.png" },
-          {    id : 6666666  ,  rating : "6666666"  ,   price : "4444444" , name :  "iphone 7"  , image : "/images/iPhone 7.jpg" },
+          {   id : 1111111   ,  rating : "8888888"  ,   price : "9999999"  ,  name : "iphone 13"  , image : "images/iPhone 13.jpg" },
+          {   id : 22222222  ,  rating : "2222222"  ,   price : "8888888"  ,  name : "iphone 11"  , image : "images/iPhone  11.jpg" },
+          {   id : 33333333  ,  rating : "3333333"  ,   price : "7777767"  ,  name : "iphone 12"  , image : "/images/iPhone 12.jpg" },
+          {   id : 4444444   ,  rating : "444444"   ,   price : "6666666"  ,  name : "iphone 10" , image : "/images/iPhone 10.png" },
+          {   id : 5555555   ,  rating : "5555555"  ,   price : "5555555"  ,  name :  "iphone 8" , image : "/images/iPhone 8.png" },
+          {    id : 6666666  ,  rating : "6666666"  ,   price : "4444444"  ,  name :  "iphone 7"  , image : "/images/iPhone 7.jpg" },
         ] 
            setlistofproduct(list);
    },[]);
