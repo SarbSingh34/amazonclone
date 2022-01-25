b307822024affd936bf264afb4990a0df67be31a
Sarbkalyan Singh <mrsinghbusiness05@gmail.com>
2022-01-25 09:59:35 +0530

    uploaded

--- src/Placeorderpage/Placeorder.js
+++ src/Placeorderpage/Placeorder.js
@@ -1,59 +1,150 @@
-import  { useState , useEffect } from 'react'; 
-import { Link, useParams } from  "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
-const Placeorder = () => 
-{
-    const[productdetails,setproductdetails] = useState([]);
-    let { id } = useParams();
+const Placeorder = () => {
+  const [productdetails, setproductdetails] = useState([]);
+  let { id } = useParams();
 
-    useEffect(() =>
-    {
-            let list = 
-         [
-           {   id : 1111111   ,  rating : "8888888"  ,   price : "9999999" , name : "iphone 13" , image : "images/Img6.webp" },
-           {   id : 22222222  ,  rating : "2222222"  ,   price : "8888888" , name : "iphone 12" , image : "images/Img5.webp" },
-           {   id : 33333333  ,  rating : "3333333"  ,   price : "7777767" , name : "iphone 11" , image : "images/Img3.webp" },
-           {   id : 4444444  ,   rating : "4444444"  ,   price : "6666666" , name : "iphone 10" , image : "images/Img9.webp" },
-           {   id : 5555555  ,  rating : "5555555"  ,   price : "5555555" , name :  "iphone 8"  , image : "images/Img4.webp" },
-           {   id : 6666666  ,  rating : "6666666"  ,   price : "4444444" , name :  "iphone 7"  , image : "images/Img7.webp" },
-         ] 
+  useEffect(() => {
+    let list = [
+      {
+        "id": "11111111",
+        "name" : "iPhone 11",
+        "rating" : "99999" ,
+        "review": "10000",
+        "emi": "24000",
+        "delivery ": "Wed 18, Aug",
+        "price": "55555",
+        "status ": "In Stock",
+        "soldby": "Drshitaa",
+        "about": [
+          "About this Camera: Sony IMX 766 50MP+8MP+2MP AI Triple Camera with 4K@30FPS|1080p video at 30/60 fps | 32MP Front camera with 1080p video at 30/60 fps | Super Slow Motion: 1080p video at 120 fps, 720p video at 240 fps | Time-Lapse: 1080p 120fps;720p 240fps Chipset: MediaTek Dimensity 1200-AI - The octa-core Dimensity 1200-AI features one of the fastest smartphone CPUs ever:",
+        ],
+        image: "images/Img6.webp",
+      },
+      {
+        "id": "2222222",
+        "name" : "iPhone 10",
+        "rating" : "99999" ,
+        "review": "10000",
+        "emi": "24000",
+        "delivery ": "Wed 18, Aug",
+        "price": "55555",
+        "status ": "In Stock",
+        "soldby": "Drshitaa",
+        "about": [
+          "About this Camera: Sony IMX 766 50MP+8MP+2MP AI Triple Camera with 4K@30FPS|1080p video at 30/60 fps | 32MP Front camera with 1080p video at 30/60 fps | Super Slow Motion: 1080p video at 120 fps, 720p video at 240 fps | Time-Lapse: 1080p 120fps;720p 240fps Chipset: MediaTek Dimensity 1200-AI - The octa-core Dimensity 1200-AI features one of the fastest smartphone CPUs ever:",
+        ],
 
-         let item = list.filter(item =>
-         {
-               if(item.id == id)
-               return item;
-         })
+        image: "images/Img6.webp",
+      },
+      {
+        "id": "33333333",
+        "name" : "iPhone 9",
+        "rating" : "99999" ,
+        "review": "10000",
+        "emi": "24000",
+        "delivery ": "Wed 18, Aug",
+        "price": "55555",
+        "status ": "In Stock",
+        "soldby": "Drshitaa",
+        "about": [
+          "About this Camera: Sony IMX 766 50MP+8MP+2MP AI Triple Camera with 4K@30FPS|1080p video at 30/60 fps | 32MP Front camera with 1080p video at 30/60 fps | Super Slow Motion: 1080p video at 120 fps, 720p video at 240 fps | Time-Lapse: 1080p 120fps;720p 240fps Chipset: MediaTek Dimensity 1200-AI - The octa-core Dimensity 1200-AI features one of the fastest smartphone CPUs ever:",
+        ],
+
+        image: "images/Img6.webp",
+      },
+      {
+        "id": "44444444",
+        "name" : "iPhone 8",
+        "rating" : "99999" ,
+        "review": "10000",
+        "emi": "24000",
+        "delivery ": "Wed 18, Aug",
+        "price": "55555",
+        "status ": "In Stock",
+        "soldby": "Drshitaa",
+        "about": [
+          "About this Camera: Sony IMX 766 50MP+8MP+2MP AI Triple Camera with 4K@30FPS|1080p video at 30/60 fps | 32MP Front camera with 1080p video at 30/60 fps | Super Slow Motion: 1080p video at 120 fps, 720p video at 240 fps | Time-Lapse: 1080p 120fps;720p 240fps Chipset: MediaTek Dimensity 1200-AI - The octa-core Dimensity 1200-AI features one of the fastest smartphone CPUs ever:",
+        ],
+
+        image: "images/Img6.webp",
+      },
+      {
+        "id": "5555555",
+        "name" : "iPhone 7",
+        "rating" : "99999" ,
+        "review": "10000",
+        "emi": "24000",
+        "delivery ": "Wed 18, Aug",
+        "price": "55555",
+        "status ": "In Stock",
+        "soldby": "Drshitaa",
+        "about": [
+          "About this Camera: Sony IMX 766 50MP+8MP+2MP AI Triple Camera with 4K@30FPS|1080p video at 30/60 fps | 32MP Front camera with 1080p video at 30/60 fps | Super Slow Motion: 1080p video at 120 fps, 720p video at 240 fps | Time-Lapse: 1080p 120fps;720p 240fps Chipset: MediaTek Dimensity 1200-AI - The octa-core Dimensity 1200-AI features one of the fastest smartphone CPUs ever:",
+        ],
+
+        image: "images/Img6.webp",
+      },
+      {
+        "id": "666666",
+        "name" : "iPhone 6",
+        "rating" : "99999" ,
+        "review": "10000",
+        "emi": "24000",
+        "delivery ": "Wed 18, Aug",
+        "price": "55555",
+        "status ": "In Stock",
+        "soldby": "Drshitaa",
+        "about": [
+          "About this Camera: Sony IMX 766 50MP+8MP+2MP AI Triple Camera with 4K@30FPS|1080p video at 30/60 fps | 32MP Front camera with 1080p video at 30/60 fps | Super Slow Motion: 1080p video at 120 fps, 720p video at 240 fps | Time-Lapse: 1080p 120fps;720p 240fps Chipset: MediaTek Dimensity 1200-AI - The octa-core Dimensity 1200-AI features one of the fastest smartphone CPUs ever:",
+        ],
+
+        image: "images/Img6.webp",
+      },
+    ];
+
+    let item = list.filter((item) => {
+      if (item.id == id) return item;
+    });
          console.log(item);
-            setproductdetails(list);
-    },[]);
-
+         setproductdetails(item[0]);
+  }, []);
 
   return (
-      <>
-         <div className = "placeorder_pg" style = {{width:'100%',display:'flex'}}>
-             <div className="first_part" style= {{width:'40%'}} >
-                     <img src = "/images/Img6.webp" style = {{width:'55%',margin:'5% 22%'}} /> 
-             </div>
-             <div className="second_part" style= {{width:'40%',backgroundColor:'blue'}}>
-                   <div className="second_header">
-                        <span>New Apple iPhone 11 (64GB) - Green  </span>
-                        <span>  35,990  ratings | 1000+ answered questions </span>
-                   </div>
-                   <div className="second_main">
-                       <span> Price: 50,999.00 </span>
-                   </div>
-             </div>
-             <div className="third_part" style= {{width:'20%', backgroundColor:'lightblue'}}>
-                    <span> Content  </span>
-                    <Link to ="/checkout">
-                        <button type ="submit" > Add to Cart </button>
-                    </Link>
-                  
-                    <button type ="submit">  Buy Now   </button>
-             </div>
-         </div>
-      </>
-  )
+    <>
+      <div className="placeorder_pg" style={{ width: "100%", display: "flex" }}>
+        <div className="first_part" style={{ width: "40%" }}>
+          <img
+            src="/images/Img6.webp"
+            style={{ width: "55%", margin: "5% 22%" }}
+          />
+        </div>
+        <div
+          className="second_part"
+          style={{ width: "40%", backgroundColor: "blue" }}>
+          <div className="second_header">
+            <span> {productdetails.name} </span>
+            <span> 35,990 ratings | 1000+ answered questions </span>
+          </div>
+          <div className="second_main">
+            <span> Price: 50,999.00 </span>
+          </div>
+        </div>
+        <div
+          className="third_part"
+          style={{ width: "20%", backgroundColor: "lightblue" }}
+        >
+          <span> Content </span>
+          <Link to="/checkout">
+            <button type="submit"> Add to Cart </button>
+          </Link>
+
+          <button type="submit"> Buy Now </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Placeorder;
